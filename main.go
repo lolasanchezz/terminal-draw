9c0e759d2c417bc0ce527abf3c4f852362732285
lolasanchezz <lolansanchez@icloud.com>
2025-12-09 22:03:14 -0500

    first toolbar made but not working

--- main.go
+++ main.go
@@ -6,21 +6,27 @@ import (
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/x/term"
 )
 
 type model struct {
-	mouseX int
-	mouseY int
-	width  int
-	height int
-	matrix [][]string
-	click  string
+	mouseX   int
+	mouseY   int
+	width    int
+	height   int
+	matrix   [][]string
+	clicking bool
+	brush    string
+	toolbar  toolbarModel
 }
 type initCmd struct{}
 
+var cursorStyle = lipgloss.NewStyle().Foreground(lipgloss.Color(0))
+
 func main() {
 	m := model{}
+	m.brush = "#"
 	if _, err := tea.NewProgram(m, tea.WithMouseAllMotion(), tea.WithAltScreen()).Run(); err != nil {
 		fmt.Println("Error running program:", err)
 		os.Exit(1)
@@ -31,10 +37,24 @@ func (m model) Init() tea.Cmd {
 	return func() tea.Msg { return initCmd{} }
 }
 
+func makeMatrix(w int, h int) [][]string {
+	matrix := make([][]string, h)
+	for i := range h {
+		matrix[i] = make([]string, w)
+		for j := range w {
+			matrix[i][j] = " "
+			if j == w-1 {
+				matrix[i][j] = "\n"
+			}
+		}
+	}
+	return matrix
+}
+
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var _ tea.Cmd
-	m.click = "not click"
 	switch msg := msg.(type) {
+
 	case initCmd:
 		w, h, err := term.GetSize(os.Stdout.Fd())
 		if err != nil {
@@ -43,34 +63,61 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		m.width = w
 		m.height = h
 		if w > 0 && h > 0 {
-			m.matrix = make([][]string, h)
-			for i := range h {
-				m.matrix[i] = make([]string, w)
-				for j := range w {
-					m.matrix[i][j] = " "
-					if j == w-1 {
-						m.matrix[i][j] = "\n"
-					}
-				}
-			}
+			m.matrix = makeMatrix(w, h-m.toolbar.height-1)
 		}
+
+		//setting up toolbar
+		//PARAMS
+		padding := 2
+		height := 5
+		m.toolbar.height = height
+		m.toolbar.width = w - (2 * padding)
+		toolbarStyle.Width(w - (2 * padding))
 	case tea.MouseMsg:
 		m.mouseX = msg.X
 		m.mouseY = msg.Y
 		switch msg.Action {
 		case tea.MouseActionPress:
-			m.click = "\npressing"
-		case tea.MouseActionMotion:
-			m.click = "\nmoving"
-			m.matrix[m.mouseY][m.mouseX] = "#"
+			//checking whether the action was within toolbar or easel
+			if msg.X > m.width+1 {
+			}
+			m.clicking = true
 		case tea.MouseActionRelease:
-			m.click = "\nrelease"
+			m.clicking = false
+		case tea.MouseActionMotion:
+			//checking whether the action was within toolbar or easel
+			if msg.X < len(m.matrix[0]) { //just in case
+				if msg.Y < m.toolbar.height+1 {
+
+				} else {
+					if m.clicking {
+						if msg.Y < len(m.matrix) { // you never know ... could be a weird mouse spasm that would cause out of bounds and crash, so this is just in case
+							m.matrix[m.mouseY+1+m.toolbar.height][m.mouseX] = cursorStyle.Render(m.brush)
+						}
+					}
+				}
+			}
+
 		}
 
 	case tea.KeyMsg:
+
 		switch msg.Type {
 		case tea.KeyCtrlC:
 			return m, tea.Quit
+
+		}
+		switch msg.String() {
+		case "r":
+			cursorStyle = cursorStyle.Foreground(lipgloss.Color("#ff0000"))
+		case "w":
+			cursorStyle = cursorStyle.Foreground(lipgloss.Color("#ffffffff"))
+		case "e":
+			m.brush = " "
+		case "b":
+			m.brush = "#"
+		case "c":
+			m.matrix = makeMatrix(m.width, m.height-m.toolbar.height)
 		}
 	}
 	return m, nil
@@ -78,22 +125,52 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 func (m model) View() string {
 	final := ""
-	for i := range m.height {
-		for j := range m.width {
+	for i := range len(m.matrix) {
+		for j := range len(m.matrix[0]) {
 			final += m.matrix[i][j]
 		}
 	}
-	return final
-	/*
-		return "\nmouse x: " +
-			strconv.Itoa(m.mouseX) +
-			"\nmouse y:" +
-			strconv.Itoa(m.mouseY) +
-			"\nwidth " +
-			strconv.Itoa(m.width) +
-			"\nheight " +
-			strconv.Itoa(m.height) +
-			m.click
-	*/
+	return lipgloss.JoinVertical(lipgloss.Center, m.toolbarView(), final)
+}
 
+var toolbar = struct {
+	colors  map[string]string
+	strokes []string
+	width   []string
+}{
+	colors: map[string]string{
+		"red":   "#ff0000",
+		"blue":  "#0000ff",
+		"green": "#00ff00"},
+	strokes: []string{"#", ".", "-"},
+	width:   []string{"◼", "◼◼", "◼◼◼"},
+}
+
+type toolbarModel struct {
+	width  int
+	height int
+}
+
+var toolbarStyle = lipgloss.NewStyle().Border(lipgloss.NormalBorder())
+
+func (m model) toolbarView() string {
+	// colors
+	colorChar := "⬤"
+	colorStr := ""
+	for _, color := range toolbar.colors {
+		colorStr = colorStr + lipgloss.NewStyle().Foreground(lipgloss.Color(color)).Render(colorChar)
+	}
+
+	strokeStr := ""
+
+	for _, stroke := range toolbar.strokes {
+		strokeStr = strokeStr + " " + stroke
+	}
+
+	widthStr := ""
+	for _, width := range toolbar.width {
+		widthStr = widthStr + " " + width
+	}
+	return "    erawera"
+	return toolbarStyle.Render(lipgloss.JoinHorizontal(lipgloss.Center, colorStr, strokeStr, widthStr))
 }
